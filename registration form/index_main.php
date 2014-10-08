3759c116bd7d44afe51256460692bacacb7ff214
ksaur24 <ksaur24@gmail.com>
2014-10-07 21:03:20 -0400

    Registration Form

---
+++ registration form/index_main.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+<a href="registration/login.php"><input type="button" value="Login"></a>
+<a href="registration/register.php"><input type="button" value="Register"></a>
+</body>
+</html>
