68a3531a8513cbba7b87100fe6c399ee54c5764f
ksaur24 <ksaur24@gmail.com>
2014-10-13 21:43:19 -0400

    access denied page

---
+++ dynamic-front-end/access_denied.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Access_Denied</title>
+</head>
+
+<body>
+<h1>Access Denied!<br>Please Try Again</h1>
+<a href="login.php"><input type="button" value="Login"></a>
+<p>Or Register if you are not a member</p>
+<a href="register.php"><input type="button" value="Register"></a>
+</body>
+</html>
