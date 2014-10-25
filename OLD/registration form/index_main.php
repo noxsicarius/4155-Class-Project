49f2d2bf9728f5884ed6d4a6ac6bbf30db84fe79
noxsicarius <sanderrick2000@yahoo.com>
2014-10-25 14:31:11 -0400

    Removed old site

--- OLD/registration form/index_main.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head></head>
-<body>
-<a href="registration/login.php"><input type="button" value="Login"></a>
-<a href="registration/register.php"><input type="button" value="Register"></a>
-</body>
-</html>
