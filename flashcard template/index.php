659fef6be810030846486a37b9959414a0cc7d80
noxsicarius <sanderrick2000@yahoo.com>
2014-10-25 14:32:00 -0400

    Adding flashcard template
    Posting up the flashcard template for everyone to look at.

---
+++ flashcard template/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Flash Cards PHP</title>
+<style type="text/css">
+<!--
+.style1 {font-size: 36px}
+.style2 {
+	font-size: 16px;
+	color: #999999;
+}
+.style3 {color: #999999}
+a:link {
+	color: #00CCFF;
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+	color: #993366;
+}
+a:hover {
+	text-decoration: underline;
+	color: #66FF00;
+}
+a:active {
+	text-decoration: none;
+	color: #FF00FF;
+}
+.style18 {	color: #0066FF;
+	font-size: 16px;
+}
+-->
+</style></head>
+
+<body>
+<div align="center">
+  <p class="style1"><img src="images/logo.jpg" alt="Flash Cards PHP" width="383" height="59" /></p>
+
+  <p class="style1">Please Select a Module:</p>
+  <table width="200" border="0" cellspacing="2" cellpadding="2">
+    <tr>
+      <td><ul>
+        <li><a href="flash.php?table=addition">Addition</a></li>
+        <li><a href="flash.php?table=subtraction">Subtraction</a></li>
+        <li><a href="flash.php?table=multiplication">Multiplication</a></li>
+        <li><a href="flash.php?table=division">Division</a></li>
+      </ul>      </td>
+    </tr>
+  </table>
+  <hr />
+  <table width="598" border="0" cellspacing="2" cellpadding="2">
+  <tr>
+    <td><div align="center"><a href="http://sourceforge.net/donate/index.php?group_id=257198"><img src="http://images.sourceforge.net/images/project-support.jpg" width="88" height="32" border="0" alt="Support This Project" /> </a></div></td>
+  </tr>
+  <tr>
+    <td><div align="center"><span class="style18">I develop this software as a way to contribute to the education of children who need help with their basic math skills. As always this software will be available free of charge, but if you find it useful donations would be very welcome. I will in turn donate 5% of anything donated to myself to Sourceforge.Net and any remaining monies will be used to better this software and the quality of life of my daughter. Thank you for your interest in this project.</span></div></td>
+  </tr>
+</table>
+</div>
+</body>
+</html>
