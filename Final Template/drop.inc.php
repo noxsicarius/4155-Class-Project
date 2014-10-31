e2e0e69f283d85ccd536d2939a26da11c75f780c
ASIF SUBHAN <SUBHAN.ASIFALI@GMAIL.COM>
2014-10-31 01:31:29 -0400

    working with droping table
    Do not change the code in this file (drop.inc.php)

---
+++ Final Template/drop.inc.php
@@ -0,0 +1,9 @@
+<?php
+    include 'connect.inc.php';
+	$name='table_2'
+    echo ('DROP TABLE IF EXISTS `a_database`.`$name`')
+	function drop_table($name){
+	//mysql_query('DROP TABLE IF EXISTS `a_database`.`$name`');	
+	}
+    
+    ?>
