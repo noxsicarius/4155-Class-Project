509a9b2f04325c66a130463bf87914c998fa7059
ASIF SUBHAN <SUBHAN.ASIFALI@GMAIL.COM>
2014-10-31 01:04:51 -0400

    bugs

--- Final Template/Database_files/matches.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE uploadInfo (
-		FileID				 int             NOT NULL,
-		Sentences            MEDIUMBLOB      NOT NULL,
-		Keywords             MEDIUMBLOB      NOT NULL,
-		MatchedTo
-
-
-
-	)
