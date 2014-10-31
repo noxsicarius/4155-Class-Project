6d319bfb6d7f0335dd300823486ca2b03f16855c
ASIF SUBHAN <SUBHAN.ASIFALI@GMAIL.COM>
2014-10-31 03:28:44 -0400

    Footer of the page in different file
    i did the same thing i did with aside and menu. just update the
    footer.php and footer in all php will be updated

--- Final Template/register.php
+++ Final Template/register.php
@@ -124,37 +124,7 @@ else if(loggedin()){
     	<div class="clear"></div>
   </div>
     <footer>
-        <div class="footer-content">
-            <ul>
-            	<li><h4>Proin accumsan</h4></li>
-                <li><a href="#">Rutrum nulla a ultrices</a></li>
-                <li><a href="#">Blandit elementum</a></li>
-                <li><a href="#">Proin placerat accumsan</a></li>
-                <li><a href="#">Morbi hendrerit libero </a></li>
-                <li><a href="#">Curabitur sit amet tellus</a></li>
-            </ul>
-            
-            <ul>
-            	<li><h4>Condimentum</h4></li>
-                <li><a href="#">Curabitur sit amet tellus</a></li>
-                <li><a href="#">Morbi hendrerit libero </a></li>
-                <li><a href="#">Proin placerat accumsan</a></li>
-                <li><a href="#">Rutrum nulla a ultrices</a></li>
-                <li><a href="#">Cras dictum</a></li>
-            </ul>
-            
-            <ul class="endfooter">
-            	<li><h4>Suspendisse</h4></li>
-                <li><a href="#">Morbi hendrerit libero </a></li>
-                <li><a href="#">Proin placerat accumsan</a></li>
-                <li><a href="#">Rutrum nulla a ultrices</a></li>
-                <li><a href="#">Curabitur sit amet tellus</a></li>
-                <li><a href="#">Donec in ligula nisl.</a></li>
-            </ul>
-            
-            <div class="clear"></div>
-        </div>
-        <div class="footer-bottom">Group Two Capstone Project</div>
+        <?php include 'footer.php' ?>;
     </footer>
 </div>
 </body>
--- Final Template/uploads.php
+++ Final Template/uploads.php
@@ -104,10 +104,10 @@ if(loggedin()){
 }
 $query = "SELECT * FROM uploadinfo WHERE  StudentID = '$StudentID'";
 if($resulta  = mysql_query($query)){
-$num_of_rows=mysql_num_rows($resulta);
-for($i=0;$i<$num_of_rows;$i++){
-$content=mysql_result($resulta,$i,"FileName");
-	$File_names[$i]= $content;
+	$num_of_rows=mysql_num_rows($resulta);
+	for($i=0;$i<$num_of_rows;$i++){
+	$content=mysql_result($resulta,$i,"FileName");
+		$File_names[$i]= $content;
 	
 }
 }
@@ -195,37 +195,7 @@ else{
     	<div class="clear"></div>
   </div>
     <footer>
-        <div class="footer-content">
-            <ul>
-            	<li><h4>Proin accumsan</h4></li>
-                <li><a href="#">Rutrum nulla a ultrices</a></li>
-                <li><a href="#">Blandit elementum</a></li>
-                <li><a href="#">Proin placerat accumsan</a></li>
-                <li><a href="#">Morbi hendrerit libero </a></li>
-                <li><a href="#">Curabitur sit amet tellus</a></li>
-            </ul>
-            
-            <ul>
-            	<li><h4>Condimentum</h4></li>
-                <li><a href="#">Curabitur sit amet tellus</a></li>
-                <li><a href="#">Morbi hendrerit libero </a></li>
-                <li><a href="#">Proin placerat accumsan</a></li>
-                <li><a href="#">Rutrum nulla a ultrices</a></li>
-                <li><a href="#">Cras dictum</a></li>
-            </ul>
-            
-            <ul class="endfooter">
-            	<li><h4>Suspendisse</h4></li>
-                <li><a href="#">Morbi hendrerit libero </a></li>
-                <li><a href="#">Proin placerat accumsan</a></li>
-                <li><a href="#">Rutrum nulla a ultrices</a></li>
-                <li><a href="#">Curabitur sit amet tellus</a></li>
-                <li><a href="#">Donec in ligula nisl.</a></li>
-            </ul>
-            
-            <div class="clear"></div>
-        </div>
-        <div class="footer-bottom">Group Two Capstone Project</div>
+        <?php include 'footer.php' ?>;
     </footer>
 </div>
 </body>
