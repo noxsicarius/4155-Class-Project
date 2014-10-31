005c928798c20b2a04856deb712ffca33f05a93a
ASIF SUBHAN <SUBHAN.ASIFALI@GMAIL.COM>
2014-10-31 03:15:04 -0400

    Revert "bugs"
    This reverts commit 5971565aa4ff9907f0133c8697325b9f086adb57.

--- Final Template/mypages.php
+++ Final Template/mypages.php
@@ -1,7 +1,17 @@
 <?php
-    include 'connect.inc.php';
+    global $link;
+    global $result;
+    global $select_db;
+    $link = mysql_connect('localhost', 'root', '');
+    if (!$link){
+        die("Database Connection Failed" . mysql_error());
+    }
+    $select_db = mysql_select_db('a_database');
+    if (!$select_db){
+        die("Database Selection Failed" . mysql_error());
+    }
     $option = '';
-    $sql = "SELECT * FROM `uploadinfo`";
+    $sql = "SELECT * FROM `uploadInfo`";
     $result = mysql_query($sql);
 
     while ($row = mysql_fetch_assoc($result)){
