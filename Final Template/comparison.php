a55ce6ec59db80497209dd8baaa4df16fb96ec02
ASIF SUBHAN <SUBHAN.ASIFALI@GMAIL.COM>
2014-11-06 09:51:59 -0500

    corrected file Name

--- Final Template/comparison.php
+++ Final Template/comparison.php
@@ -80,24 +80,40 @@
 	//ST_CompareFileTOMaster(4);
 	//Master is the Class StudyGuide
 	ST_CompareFileTOMaster(58);
+	
+	
+	
 	Function ST_CompareFileTOMaster($FileID){
 		$School=FileInfo($FileID,'School');	$School=strtolower($School);		
 		$Class=FileInfo($FileID,'ClassName'); $Class=strtolower($Class);
 		$Search=ST_SearchClass($School,$Class);
-		echo $Search;
+		$Sentences=ST_GetFileSentences($FileID);
+		$Keywords=ST_GetFileKeywords($FileID);
+		//Check if the 
 		if($Search==0){
 			echo 'Inside If ';
 			ST_CreateClassTable($School,$Class);
-			$Sentences=ST_GetFileSentences($FileID);
-			$Keywords=ST_GetFileKeywords($FileID);
+			
 			for($x=0;$x<sizeof($Sentences);$x++){
 				ST_WriteToClass($School,$Class,$Sentences[$x],$Keywords[$x]);				
 			}
-		}
-		
-	
-		
-	
+			
+		}else{			
+			$Master=ST_GetClassKeyWords($School,$Class);
+			$size=sizeof($Master);
+			$sizeFile=sizeof(ST_GetFileKeywords($FileID))+1;
+			
+			//echo $size.' '.	$sizeFile;
+			for($x=0;$x<$size;$x++){ //x: size of master sentences				
+				for($y=1;$y<$sizeFile;$y++){ //y: size of File sentences
+					$temp_Master=ST_GetMasterSenKeywords($School,$Class,$x);
+					$temp_File=ST_GetSenKeywords($FileID,$y);
+					echo 'Comparing Master sentence '.$x.' to File Sentence '.$y.'<br>';
+					$percentage=ST_CompareTwoSentences($temp_File,$temp_Master);
+					echo $percentage.'<br> ';
+				}
+			}
+		}	
 	}
 	
 	
@@ -114,7 +130,7 @@
 	
 	//-----------------------------------------------Working with Tables------------------------------------------------------------------------------
 	Function ST_CreateClassTable($university,$class){
-		$String='CLASS_'.$university.'_'.$class;
+		$String='class_'.$university.'_'.$class;
 		$query="CREATE TABLE IF NOT EXISTS `$String` (
 				  `SentenceNo` int(11) NOT NULL AUTO_INCREMENT,
 				  `Keywords` text NOT NULL,
@@ -152,7 +168,7 @@
 	//if found returns 1 else returns 0
 	
 	Function ST_SearchClass($School,$class){
-		echo $School. ' '.$class;
+		//echo $School. ' '.$class;
 		$Array=ST_AllCLasses();
 		for($x=0;$x<sizeof($Array);$x++){
 			if($School==$Array[$x][0] && $class==$Array[$x][1]){
@@ -163,20 +179,44 @@
 	}
 	
 	//Returns table name for a class and school
-	Function ST_ClassTableName($School,$class){
-		$String='class_'.$School.'_'.$class;
+	Function ST_ClassTableName($School,$Class){
+		$String='class_'.$School.'_'.$Class;
 		return $String;	
 	}
 	
 	//Writes to Class table
 	Function ST_WriteToClass($School,$class,$sentence,$keywords){
 		$TableName=ST_ClassTableName($School,$class);
-		
 		$database=DatabaseName();
 		$query="INSERT INTO `$database`.`$TableName` VALUES (NULL, '$keywords', '$sentence', '0')";
 		mysql_query($query);
 	
-	}	
+	}
+	
+	//Pulls keywords from Class table
+	Function ST_GetClassKeyWords($School,$Class){
+		$tablename=$String='class_'.$School.'_'.$Class;
+		$query="SELECT * FROM `$tablename`";
+		if($result = mysql_query($query)){
+			$num_of_rows=mysql_num_rows($result);
+			for($i=0;$i<$num_of_rows;$i++){
+				$content=mysql_result($result,$i,'Keywords');
+				$File_Field[$i]= $content;
+			}
+			return $File_Field;
+		}
+	}
+	//pulls keywords from each sentence in the class table
+	
+	Function ST_GetMasterSenKeywords($School,$Class,$SentenceNo){
+		$ALlFileKeywords=ST_GetClassKeyWords($School,$Class);
+		$NoSen=sizeof($ALlFileKeywords);
+		if($SentenceNo<$NoSen){
+			$SentenceKeywords=preg_split('/,/',$ALlFileKeywords[$SentenceNo]);
+			return $SentenceKeywords;
+		}
+		
+	}
 	
 	
 ?>
