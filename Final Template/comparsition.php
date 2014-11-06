ce732fd6050901115529cd1d96b33642e162e116
ASIF SUBHAN <SUBHAN.ASIFALI@GMAIL.COM>
2014-11-05 22:17:46 -0500

    BUGS

---
+++ Final Template/comparsition.php
@@ -0,0 +1,182 @@
+<?php
+	require 'connect.inc.php';
+	require 'core.inc.php';
+	
+	//print_r(ST_GetFileKeywords(4));
+	//echo ST_NoOfSentences(4);
+	//print_r(ST_GetSenKeywords(4,1));
+	//$First=ST_GetSenKeywords(5,1);
+	//$Second=ST_GetSenKeywords(4,1);
+	//echo ST_CompareTwoSentences($First,$Second);
+	//ST_CreateClassTable('uncg','1604'); ST_CreateClassTable('uncp','1904');
+	
+	//Get the number of sentences in a file.
+	Function  ST_NoOfSentences($FileID){
+		$Number=sizeof(ST_GetFileKeywords($FileID));
+		return $Number;
+	}
+	
+	//Pass the FileID and Returns the Array of File Keywords as a String per sentence 
+	Function  ST_GetFileKeywords($FileID){
+		$tablename='table_'.$FileID;
+		$query="SELECT * FROM `$tablename`";
+		if($result = mysql_query($query)){
+			$num_of_rows=mysql_num_rows($result);
+			for($i=0;$i<$num_of_rows;$i++){
+				$content=mysql_result($result,$i,'Keywords');
+				$File_Field[$i]= $content;
+			}
+			return $File_Field;
+		}
+		
+	}
+	
+	function ST_GetFileSentences($FileID){
+		$tablename='table_'.$FileID;
+		$query="SELECT * FROM `$tablename`";
+		if($result = mysql_query($query)){
+			$num_of_rows=mysql_num_rows($result);
+			for($i=0;$i<$num_of_rows;$i++){
+				$content=mysql_result($result,$i,'Sentence');
+				$File_Field[$i]= $content.'.';
+			}
+			return $File_Field;
+		}
+	}
+	// Return an Array of keywords of a specific sentence
+	//Pass FIleID and Sentence Number
+	Function ST_GetSenKeywords($FileID,$SentenceNo){
+		$ALlFileKeywords=ST_GetFileKeywords($FileID);
+		$NoSen=ST_NoOfSentences($FileID);
+		if($SentenceNo>!$NoSen){
+			$SentenceNo--;
+			$SentenceKeywords=preg_split('/,/',$ALlFileKeywords[$SentenceNo]);
+		}
+		return $SentenceKeywords;
+	}
+	// Pass two array and it will return the percentage of how many elements matched 
+	//Pass arrays of sentence keywords
+	Function ST_CompareTwoSentences($First,$Second){
+		$count=0;
+		for($x=0;$x<sizeof($First);$x++){
+			for($y=0;$y<sizeof($Second);$y++){
+				if($First[$x]==$Second[$y]){
+					$count++;
+					break;
+				}
+				
+			}	
+		}
+		if($count>0){
+			$average=(sizeof($First)+sizeof($Second))/2;
+			$percentage=($count/$average)*100;
+			$percentage=round($percentage,2);
+			return $percentage;
+		}else{
+			return 0;
+		}		
+	}
+	
+	//ST_CompareFileTOMaster(4);
+	//Master is the Class StudyGuide
+	ST_CompareFileTOMaster(58);
+	Function ST_CompareFileTOMaster($FileID){
+		$School=FileInfo($FileID,'School');	$School=strtolower($School);		
+		$Class=FileInfo($FileID,'ClassName'); $Class=strtolower($Class);
+		$Search=ST_SearchClass($School,$Class);
+		echo $Search;
+		if($Search==0){
+			echo 'Inside If ';
+			ST_CreateClassTable($School,$Class);
+			$Sentences=ST_GetFileSentences($FileID);
+			$Keywords=ST_GetFileKeywords($FileID);
+			for($x=0;$x<sizeof($Sentences);$x++){
+				ST_WriteToClass($School,$Class,$Sentences[$x],$Keywords[$x]);				
+			}
+		}
+		
+	
+		
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//-----------------------------------------------Working with Tables------------------------------------------------------------------------------
+	Function ST_CreateClassTable($university,$class){
+		$String='CLASS_'.$university.'_'.$class;
+		$query="CREATE TABLE IF NOT EXISTS `$String` (
+				  `SentenceNo` int(11) NOT NULL AUTO_INCREMENT,
+				  `Keywords` text NOT NULL,
+				  `Sentence` text NOT NULL,
+				  `Hits` int(11) NOT NULL,
+				  PRIMARY KEY (`SentenceNo`)
+				)";
+		mysql_query($query);
+	}
+
+
+	//return two dim array of all the classes in the database
+	//Array has two columns first school name and second class name
+	//Array has multiple rows depends on the number of the classes
+	Function ST_AllCLasses(){
+		$Array=array();$count=0;
+		$AllTables=Table_Names();
+		for($x=0;$x<sizeof($AllTables);$x++){
+			$result = substr($AllTables[$x], 0, 5);
+			if($result == 'class'){
+				$temp=preg_split('/_/',$AllTables[$x]);
+					for($y=0;$y<2;$y++){
+						$Array[$count][$y]=$temp[$y+1];
+												
+					}
+					$count++;
+				
+			}
+		}
+		return $Array;		
+		
+	}
+	
+	//This Function searches for a class and school 
+	//if found returns 1 else returns 0
+	
+	Function ST_SearchClass($School,$class){
+		echo $School. ' '.$class;
+		$Array=ST_AllCLasses();
+		for($x=0;$x<sizeof($Array);$x++){
+			if($School==$Array[$x][0] && $class==$Array[$x][1]){
+				return 1;break;
+			}
+		}
+		return 0; 
+	}
+	
+	//Returns table name for a class and school
+	Function ST_ClassTableName($School,$class){
+		$String='class_'.$School.'_'.$class;
+		return $String;	
+	}
+	
+	//Writes to Class table
+	Function ST_WriteToClass($School,$class,$sentence,$keywords){
+		$TableName=ST_ClassTableName($School,$class);
+		
+		$database=DatabaseName();
+		$query="INSERT INTO `$database`.`$TableName` VALUES (NULL, '$keywords', '$sentence', '0')";
+		mysql_query($query);
+	
+	}	
+	
+	
+?>
