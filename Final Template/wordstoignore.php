21f8c7e2c719b054ab52079f38d0bf5af7399eed
noxsicarius <sanderrick2000@yahoo.com>
2014-11-07 12:41:46 -0500

    Added words to the ignore list
    I believe apostrophes require an escape character. We need to look into
    using regular expressions though.

--- Final Template/wordstoignore.php
+++ Final Template/wordstoignore.php
@@ -1,5 +1,5 @@
 <?php
 
-	$wordstoignore=array("is","was","are","they","can","a","i");
+	$wordstoignore=array("is","was","are","and","or","they","can","a","i","you","home","from","scan","tan","my","because","isn\'t","isnt","not","will","as","such","many","i\'ve","I\'ve","ive","Ive","am","he","she","him","her","to","more","for","were","there","they\'re","theyre","their","theirs","when","too");
 	
 >
