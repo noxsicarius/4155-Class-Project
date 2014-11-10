2097c6e7062f40bda2ee632c2f1a1013a5d7528c
noxsicarius <sanderrick2000@yahoo.com>
2014-11-10 14:38:36 -0500

    Style fixes
    Fixed the footer issue caused by bootstrap.
    Fixed the incorrect table setup in yourfiles.

--- Final Template/comparefiles/yourfiles.php
+++ Final Template/comparefiles/yourfiles.php
@@ -1,41 +1,35 @@
+<br>
+	<div class="panel panel-default">
+	  <!-- Default panel contents -->
+	  <!--<div class="panel-heading">Your Files</div>-->
+		<div class="panel-heading" style=><font size="6"><b>Your Files</b></font></div>
+		<table class="table">
+			<tr>
+				<td><font size="4"><b>File Title</b></font></td>
+				<td><font size="4"><b>File Name</b></font></td>
+				<td><font size="4"><b>Action</b></font></td>
+			</tr>
 
-			<br>
-			<div class="panel panel-default">
-			  <!-- Default panel contents -->
-			  <!--<div class="panel-heading">Your Files</div>-->
-			  <h2> Your Files</h2>
-			  <div class="panel-body">
-			  <table class="table">
-										
-						<tr bgcolor="#FFFFF0">
-							
-							<td><b>File Title</b></td>
-							<td><b>File Name</b></td>
-							<td><b>Action</b></td>
-						</tr>
+			<?php
+				$ID=getuserid();
+				$Files_Title=FilesInDataBase_ID('NotesTitle',$ID);
+				$Files_Names=FilesInDataBase_ID('FileName',$ID);
+				$Files_ID=FilesInDataBase_ID('FileID',$ID);
+				$arrlength=count($Files_Title);
 
-						<?php
-							$ID=getuserid();
-							$Files_Title=FilesInDataBase_ID('NotesTitle',$ID);
-							$Files_Names=FilesInDataBase_ID('FileName',$ID);
-							$Files_ID=FilesInDataBase_ID('FileID',$ID);
-							$arrlength=count($Files_Title);
-
-							for($x=0;$x<$arrlength;$x++){
-								echo '<tr>';
-								//First column
-								echo '<td>'."$Files_Title[$x]".'</td>';
-								//Second column 
-								echo '<td>'."$Files_Names[$x]".'</td>';
-								//Third Column
-								$similarfile='similar.php?id='.$Files_ID[$x];								
-								echo '<td>';echo "<a href='".$similarfile."'>View Similar Notes</a>";
+				for($x=0;$x<$arrlength;$x++){
+					echo '<tr>';
+					//First column
+					echo '<td>'."$Files_Title[$x]".'</td>';
+					//Second column 
+					echo '<td>'."$Files_Names[$x]".'</td>';
+					//Third Column
+					$similarfile='similar.php?id='.$Files_ID[$x];								
+					echo '<td>';echo "<a href='".$similarfile."'>View Similar Notes</a>";
 								
-								echo '</td>';
-								echo '</tr>';
-							}
-						?>
-				</table>
-				</div>
-				</div>
-				
+					echo '</td>';
+					echo '</tr>';
+				}
+			?>
+	</table>
+</div>
