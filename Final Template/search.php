dc72b8f7ce01238b76f51e9896847b938f5175ed
noxsicarius <sanderrick2000@yahoo.com>
2014-11-11 23:29:35 -0500

    Advanced Search
    Added advanced search system

--- Final Template/search.php
+++ Final Template/search.php
@@ -3,19 +3,21 @@
 	require 'core.inc.php';
 	require 'connect.inc.php';
 	require 'rate.inc.php';
-
-	if(isset($_GET['searchparams'])){
-		$searchText = $_GET['searchparams'];
-		$searchResults=searchDB($searchText);
-	}
+	
+	// Initialize variables
+	$searchTitle="";
+	$searchCourse="";
+	$searchInstructor="";
+	$searchUniversity="";
 
 ?>
+
 <!doctype html>
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>X Note Plus</title>
-	<link rel="stylesheet" href="styles.css" type="text/css" />
+	<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
 </head>
 <body>
 	<div id="container">
@@ -31,20 +33,78 @@
 
 			<section id="content">
 				<br/>
-				<h2>Search By Document Title</h2>
-					<form action="search.php" method="get">
-						<label>Document Title:
-							<input type="text" name="searchparams" />
-						</label>
-						<input type="submit" value="Search" />
-					</form>
-				<br/><br/><br/>
+				<h2>Search for Documents</h2> <br>
 
-				<h2><?php if(isset($_GET['searchparams'])){echo 'Search Results: ';}?></h2>
+<!----------------------------- Normal Search ----------------------------->
+				<form action="search.php" method="get">
+					<div class="input-group">
+					  <span class="input-group-addon">Document</span>
+					  <input type="text" name="searchDoc" class="form-control" placeholder="Search document title">
+					</div><br>
+
+<!----------------------------- Advanced Search ----------------------------->
+					<?php
+						$spoilerContent= "
+						<div class='input-group'>
+						  <span class='input-group-addon'>Course</span>
+						  <input type='text' name='searchCourse' class='form-control' placeholder='Search a course'>
+						</div><br>
+						<div class='input-group'>
+						  <span class='input-group-addon'>Instructor</span>
+						  <input type='text' name='searchInstructor' class='form-control' placeholder='Search an instructor'>
+						</div><br>
+						<div class='input-group'>
+						  <span class='input-group-addon'>University</span>
+						  <input type='text' name='searchUni' class='form-control' placeholder='Search a university'>
+						</div><br>";
+						$title="Advanced-Search";
+						createSpoiler($title, $spoilerContent, $rateUp, $rateDown);
+					?>
+<!----------------------------- END Advanced Search ----------------------------->
+
+					<button class="btn btn-default" type="submit">Search</button>
+				</form><br>
+				
+				<h2><?php if(isset($_GET['searchDoc'])){echo 'Search Results: ';}?></h2>
 				
 				<?php
-					if(isset($_GET['searchparams'])){
-						if((mysql_num_rows($searchResults)) >= 1) {
+					$searchQuery = "SELECT * FROM uploadinfo WHERE ";
+					$init=1;
+					if(isset($_GET['searchDoc'])){
+						$searchTitle = $_GET['searchDoc'];
+						if($searchTitle != ""){
+							$searchQuery .= "NotesTitle LIKE '%$searchTitle%' ";
+							$init=($init==1)?0:$init;
+						}
+					}
+					if(isset($_GET['searchCourse'])){
+						$searchCourse = $_GET['searchCourse'];
+						if($searchCourse != ""){
+							if($init==1){$medium="";} else {$medium=" AND ";}
+							$searchQuery .= $medium."ClassName LIKE '%$searchCourse%' ";
+							$init=($init==1)?0:$init;
+						}
+					}
+					if(isset($_GET['searchInstructor'])){
+						$searchInstructor = $_GET['searchInstructor'];
+						if($searchInstructor != ""){
+							if($init==1){$medium="";} else {$medium=" AND ";}
+							$searchQuery .= $medium."Teacher LIKE '%$searchInstructor%' ";
+							$init=($init==1)?0:$init;
+						}
+					}
+					if(isset($_GET['searchUni'])){
+						$searchUniversity = $_GET['searchUni'];
+						if($searchUniversity != ""){
+							if($init==1){$medium="";} else {$medium=" AND ";}
+							$searchQuery .= $medium."Teacher LIKE '%$searchUniversity%' ";
+							$init=($init==1)?0:$init;
+						}
+					}
+
+					if(($searchTitle!="")||($searchCourse!="")||($searchInstructor!="")||($searchUniversity!="")){
+						$searchResults = searchDB($searchQuery);
+						if((mysql_num_rows($searchResults)) > 0) {
 							while($row = mysql_fetch_array($searchResults)) {
 								$docTitle = $row['NotesTitle'];
 								$docContent = $row['Content'];
@@ -52,15 +112,13 @@
 								createSpoiler($docTitle, $docContent, $rateUp, $rateDown);
 							}
 						} else {
-							if($searchText == "") {
-								echo "No search entered";
-							} else {
-								echo "No match found for ".$searchText;
-							}
+							echo "No match found.";
 						}
+					} else {
+						echo "No search entered";
 					}
 				?>
-
+				<br>
 			</section>
 			
 			<aside class="sidebar">
