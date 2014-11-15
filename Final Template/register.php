dcaf0d9d70109ea2b56b4692175472be133cdba3
noxsicarius <sanderrick2000@yahoo.com>
2014-11-15 03:42:41 -0500

    Register overhaul
    Fixed some aspects of register and bootstrapped it.

--- Final Template/register.php
+++ Final Template/register.php
@@ -1,49 +1,58 @@
 <?php
 	require 'core.inc.php';
 	require 'connect.inc.php';
-	$ALL_fields=' ';
-	$Not_working=' ';
-	$password_match= ' ';
-	$user_name_exit =' ';
-	$aleady_register=' ';
+
 	$logged_in=0;
-
-
+	$user_name_exists = false;
+	$password_match = true;
+	$ALL_fields = true;
+	$successful = false;
+	$username = '';
+	$password = '';
+	$password_again = '';
+	$name = '';
+	$school = '';
 
 	if(!loggedin()){
-		if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['password_again']) && isset($_POST['name']) && isset($_POST['school']) ){
+		$already_register = false;
+		if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['password_again']) && isset($_POST['name']) && isset($_POST['school'])){
 			$username = $_POST['username'];
 			$password = $_POST['password'];
 			$password_again = $_POST['password_again'];
 			$name = $_POST['name'];
 			$school = $_POST['school'];
 			
-			
 			if(!empty($username) && !empty($password) && !empty($password_again) && !empty($name) && !empty($school)){
+				$ALL_fields = true;
+
 				if($password!=$password_again){
-					$password_match= 'Password do not match.';
-				}else{
-					$query = "SELECT `users`.`username` FROM users WHERE (`users`.`username` = '$username')";
-					$query_run = mysql_query($query);
-					$num_of_rows=mysql_num_rows($query_run);
-					if($num_of_rows==1) {
-						$user_name_exit=  'The username '.$username. ' already exists.';
-					} else{
+					$password_match = false;
+				} else {
+					$password_match = true;
+				}
+				
+				$query = "SELECT `users`.`username` FROM users WHERE (`users`.`username` = '$username')";
+				$query_run = mysql_query($query);
+				$num_of_rows = mysql_num_rows($query_run);
+				
+				if($num_of_rows==1) {
+					$user_name_exists = true;
+					$successful = false;
+				} else {
+					$user_name_exists = false;
+					if($password_match){
 						$query = "INSERT INTO users VALUES (id,'".mysql_real_escape_string($username)."','".mysql_real_escape_string($password)."','".mysql_real_escape_string($name)."','".mysql_real_escape_string($school)."')";
-						//$query="INSERT INTO 'users' VALUES ('','".mysql_real_escape_string($username)."','".mysql_real_escape_string($password)."','".mysql_real_escape_string($name)."','".mysql_real_escape_string($school)."')";
 						if ($query_run = mysql_query($query)){
-							header('Location: register_success.php');
-						}else {
-							$Not_working= 'Sorry , try again later';
+							$successful = true;
 						}
 					}
 				}
 			}else{
-				$ALL_fields= 'All fields are required';
+				$ALL_fields = false;
 			}
 		}
 	} else if(loggedin()){
-		$aleady_register= 'you are already logged in, log out to register for another account';
+		$already_register=true;
 		$logged_in=1;
 	}
 
@@ -55,6 +64,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>X Note Plus</title>
 	<link rel="stylesheet" href="styles.css" type="text/css" />
+	<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
 </head>
 
 <body>
@@ -69,33 +79,82 @@
 		</nav>
 
 		<div id="body">
-
 			<section id="content">
 
-				<article>
-					<h1>Welcome to NotePlus</h1>
-				</article>
 				<br>
 				<?php
-					if($logged_in==1){
-						echo 'You are already logged in.';
+					if($already_register){ ?>
+						<div class="alert alert-warning" role="alert">
+							<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+							<span class="sr-only">Error:</span>
+							You are logged in, Log out to register for another account
+						</div>
+				<?php
 					}else {
-						echo '<form action="register.php" method="POST">
-								Username:<br> <input type="text" name ="username"><br><br>
-								Password:<br> <input type="password" name ="password"><br><br>
-								Retype Password:<br> <input type="password" name ="password_again"><br><br>
-								Full Name:<br> <input type="text" name ="name"><br><br>
-								School:<br> <input type="text" name ="school"><br><br>
-								<input type="submit" value ="Register">
+						if(isset($_POST['btnRegister']) && $successful) {
+							?>
+							<article class="expanded">
+								<div class="alert alert-success" role="alert">
+									<span class="sr-only">Welcome!</span>
+									<h2>Registration successful</h2>
+								</div>
+							</article>
+					<?php 
+						} else { 
+						?>
+							<article>
+								<h2>Registration Form</h2>
+							</article>
+						<?php
+							if($user_name_exists){ ?>
+								<div class="alert alert-danger" role="alert">
+									<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+									<span class="sr-only">Error:</span>
+									The username <?php print "$username";?> already exists
+								</div>
+							<?php
+							}
+							if(!$ALL_fields){ ?>
+								<div class="alert alert-danger" role="alert">
+									<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+									<span class="sr-only">Error:</span>
+									ALL fields are required!
+								</div>
+							<?php
+							}
+							if(!$password_match){ ?>
+								<div class="alert alert-danger" role="alert">
+									<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+									<span class="sr-only">Error:</span>
+									Passwords do not match
+								</div>
+					<?php	} ?>
 
-								</form>';
-					
-					
-						echo $ALL_fields;
-						echo $Not_working;
-						echo $password_match;
-						echo $user_name_exit;
-						echo $aleady_register;
+							<form action="register.php" method="POST">
+								<div class="input-group">
+								  <span class="input-group-addon" style='min-width:100px;'>Username</span>
+								  <input type="text" style='max-width:250px;' value="<?php if(!($username == '')){print "$username";}?>" name="username" class="form-control" placeholder="Choose a username"><br>
+								</div><br>
+								<div class="input-group">
+								  <span class="input-group-addon" style='min-width:100px;'>Password</span>
+								  <input type="password" style='max-width:250px;' name="password" class="form-control" placeholder="Create a password"><br>
+								</div><br>
+								<div class="input-group">
+								  <span class="input-group-addon" style='min-width:100px;'>Password</span>
+								  <input type="password" style='max-width:250px;' name="password_again" class="form-control" placeholder="Retype password"><br>
+								</div><br>
+								<div class="input-group">
+								  <span class="input-group-addon" style='min-width:100px;'>Full Name</span>
+								  <input type="text" style='max-width:250px;' value="<?php if(!($name == '')){print "$name";}?>" name="name" class="form-control" placeholder="Enter your full name"><br>
+								</div><br>
+								<div class="input-group">
+								  <span class="input-group-addon" style='min-width:100px;'>University</span>
+								  <input type="text" style='max-width:250px;' value="<?php if(!($school == '')){print "$school";}?>" name="school" class="form-control" placeholder="University you attend"><br>
+								</div><br>
+								<button class="btn btn-default" type="submit" name="btnRegister">Register</button>
+							</form>
+						<?php
+						}
 					}
 				?>
 			
